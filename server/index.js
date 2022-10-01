558cbeb956237d3be14a1b41c3ab5647f4865d6a
Gregdabro <gregmonak@gmail.com>
2022-10-01 10:23:09 +0200

    Created class Error and ErrorMiddleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
 const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandlingMissleware");
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+
+// Обработка ошибок, последний Middleware
+app.use(errorHandler);
 
 const start = async () => {
     try {
