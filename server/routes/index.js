ea2f1d09914ba0e6c3102d8fbf36fc419148f7b8
Gregdabro <gregmonak@gmail.com>
2022-10-01 12:56:56 +0200

    Implement CRUD

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const Router = require("express");
 const router = new Router();
-const deviceRouter = require("./deviceRoutes");
-const userRouter = require("./userRoutes");
-const brandRouter = require("./brandRoutes");
-const typeRouter = require("./typeRoutes");
+const deviceRouter = require("./deviceRouter");
+const userRouter = require("./userRouter");
+const brandRouter = require("./brandRouter");
+const typeRouter = require("./typeRouter");
 
 router.use("/user", userRouter);
 router.use("/type", typeRouter);
